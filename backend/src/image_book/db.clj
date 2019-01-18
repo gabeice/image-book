3f39c87ea5700a757af7a3b72a048e41af89c6c0
Gabriel McDonald <gabrielmcdonald1@gmail.com>
2019-01-18 12:51:01 -0500

    db outline

--- backend/src/image_book/db.clj
+++ backend/src/image_book/db.clj
@@ -1,6 +1,32 @@
-(ns image-book.db)
+(ns image-book.db
+  (:require [datomic.client.api :as d]))
+
+(def cfg {:server-type :peer-server
+          :access-key "myaccesskey"
+          :secret "mysecret"
+          :endpoint "localhost:8998"})
+
+(def client (d/client cfg))
+(def conn (d/connect client {:db-name "image-book"}))
+
+(def photo-schema [{:db/ident :photo/title
+                    :db/valueType :db.type/string
+                    :db/cardinality :db.cardinality/one
+                    :db/doc "The title of the photo"}
+                   {:db/ident :photo/url
+                    :db/valueType :db.type/string
+                    :db/cardinality :db.cardinality/one
+                    :db/doc "The url for the photo"}])
 
 (defn all-photos []
-  [])
+  (let [db (d/db conn)
+        query '[:find ?title ?url
+                :where [?e :photo/title ?title
+                        ?e :photo/url ?url]]
+        [title url] (d/q query db)]
+    {:title title
+     :url url}))
 
-(defn add-photo [image-file])
+(defn add-photo [image-file]
+  (let [uploaded-photo [{:photo/title (:title image-file)}]]
+    (d/transact conn {:tx-data uploaded-photo})))
